9789ba17ef09e446358ec362eada657d07c3c246
MJonas <mateusjonass23@gmail.com>
2021-10-15 23:21:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,20 @@ function Relogio(){
     var sec = data.getSeconds()
 
     var horat = hr + ':' + min + ':' + sec
-
     var res = window.document.getElementById('res')
     res.innerHTML = horat
+
 }
 setInterval(Relogio,500)
+
+function calendario(){
+    var data1 = new Date()
+    var ano = data1.getFullYear()
+    var mes = data1.getMonth()
+    var dia = data1.getDate()
+    mes += 1
+
+    var anot = window.document.getElementById('mes')
+    anot.innerHTML= dia + '/' + mes + '/' + ano 
+}
+setInterval(calendario,50)
