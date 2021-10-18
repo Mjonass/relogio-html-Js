10ece96e05140366e2d61bb104aeb7aaa9ac9d17
MJonas <mateusjonass23@gmail.com>
2021-10-18 17:32:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,30 @@ function calendario(){
     anot.innerHTML= dia + '/' + mes + '/' + ano 
 }
 setInterval(calendario,50)
+
+
+function diasemana(){
+     var dias = window.document.getElementById('dias')
+     var agora = new Date()
+     var diasem = agora.getDay()
+
+     if( diasem == 0){
+         dias.innerHTML= 'Domingo'
+     } else if( diasem == 1){
+         dias.innerHTML = 'Segunda'
+     } else if( diasem == 2){
+        dias.innerHTML = 'Terça'
+     } else if( diasem == 3){
+        dias.innerHTML = 'Quarta'
+     } else if( diasem == 4){
+        dias.innerHTML = 'Quinta'
+     } else if( diasem == 5){
+        dias.innerHTML = 'Sexta'
+     }else{
+        dias.innerHTML = 'Sabado'
+     }
+ 
+
+
+}
+setInterval(diasemana,50)
